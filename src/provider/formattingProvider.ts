226674b97e4bf8e2591222681e5ea21a3cb09b12
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2020-12-06 15:51:04 -0500

    format: indentation after #if, make it same as #ifwinactive
    tsconfig.json to "es2019"
    
    formattingProvider.ts : added comments, changed style, removed semicolons

--- src/provider/formattingProvider.ts
+++ src/provider/formattingProvider.ts
@@ -1,14 +1,14 @@
-import * as vscode from 'vscode';
-import { CodeUtil } from '../common/codeUtil';
+import * as vscode from 'vscode'
+import { CodeUtil } from '../common/codeUtil'
 
 function fullDocumentRange(document: vscode.TextDocument): vscode.Range {
-    const lastLineId = document.lineCount - 1;
+    const lastLineId = document.lineCount - 1
     return new vscode.Range(
         0,
         0,
         lastLineId,
         document.lineAt(lastLineId).text.length,
-    );
+    )
 }
 
 export class FormatProvider implements vscode.DocumentFormattingEditProvider {
@@ -34,147 +34,158 @@ export class FormatProvider implements vscode.DocumentFormattingEditProvider {
         options: vscode.FormattingOptions,
         token: vscode.CancellationToken,
     ): vscode.ProviderResult<vscode.TextEdit[]> {
-        let formatDocument = '';
-        let deep = 0;
-        let tagDeep = 0;
-        let oneCommandCode = false;
-        let blockComment = false;
+        let formatDocument = ''
+        let deep = 0 //deep is indentation level
+        let depthOfHotkey = 0
+        let oneCommandCode = false
+        let blockComment = false
+        let notHotkey = false
 
         for (let line = 0; line < document.lineCount; line++) {
-            const originText = document.lineAt(line).text;
+            const originText = document.lineAt(line).text
             if (originText.match(/ *\/\*/)) {
-                blockComment = true;
+                blockComment = true
             }
             if (originText.match(/ *\*\//)) {
-                blockComment = false;
+                blockComment = false
             }
             if (blockComment) {
-                formatDocument += originText;
+                formatDocument += originText
                 if (line !== document.lineCount - 1) {
-                    formatDocument += '\n';
+                    formatDocument += '\n'
                 }
-                continue;
+                continue
             }
-            const purityText = CodeUtil.purity_greedy(originText.toLowerCase());
-            // console.log(purityText)
-            let notDeep = true;
-
+            const purityText = CodeUtil.purity_greedy(originText.toLowerCase())
+            let notDeep = true
+            //start of indentation substraction
             if (
+                purityText.includes("#if") ||
                 purityText.match(/#ifwinactive$/) ||
                 purityText.match(/#ifwinnotactive$/)
             ) {
-                if (tagDeep > 0) {
-                    deep -= tagDeep;
+                if (depthOfHotkey > 0) {
+                    if (deep > 0) {
+                        deep--
+                    }
                 } else {
-                    deep--;
+                    deep--
+                    console.log("here", deep)
+
                 }
-                notDeep = false;
+                notDeep = false
             }
             // \bword\b:whole words only, /i:case insensitive
-            if (purityText.match(/\b(return)\b/i) && tagDeep === deep) {
-                tagDeep == 0; //this does nothing
-                deep--;
-                notDeep = false;
+            if (purityText.match(/\b(return)\b/i)) {
+                if (deep === depthOfHotkey) {
+                    deep--
+                    notDeep = false
+                }
             }
 
             if (purityText.match(/^\s*case.+?:\s*$/)) {
-                tagDeep--;
-                deep--;
-                notDeep = false;
+                depthOfHotkey--
+                deep--
+                notDeep = false
             } else if (purityText.match(/:\s*$/)) {
-                if (tagDeep > 0 && tagDeep === deep) {
-                    deep--;
-                    notDeep = false;
+                if (depthOfHotkey === deep) {
+                    deep--
+                    notDeep = false
                 }
             }
+            const trimmedText = purityText.trimEnd()
+            const trimmedTextLen = trimmedText.length
+            const lastChar = trimmedText.slice(trimmedTextLen - 1)
+
+            if (lastChar === ":")
+                notHotkey = false
+            else
+                notHotkey = true //mostly not hotkeys
 
             if (purityText.includes("}")) {
-                if (!purityText.includes("}:")) {
-                    deep--;
-                }
-                notDeep = false;
+                if (notHotkey) deep--
+                notDeep = false
             }
 
-            if (oneCommandCode && purityText.match(/{/) != null) {
-                let temp = purityText.match(/{/).length;
-                const t2 = purityText.match(/{[^{}]*}/);
-                if (t2) {
-                    temp = temp - t2.length;
-                }
-                if (temp > 0) {
-                    oneCommandCode = false;
-                    deep--;
+            if (oneCommandCode && purityText.includes("{")) {
+                if (notHotkey) {
+                    deep--
+                    oneCommandCode = false
                 }
             }
+
+            //end of indentation substraction
 
             if (deep < 0) {
-                deep = 0;
+                deep = 0
             }
-            let comment: any = /;.+/.exec(originText);
-            comment = comment ? comment[0] : '';
+            let comment: any = /;.+/.exec(originText)
+            comment = comment ? comment[0] : ''
 
             const formatedText =
                 originText
                     .replace(/^\s*/, '')
                     .replace(/;.+/, '')
-                    .replace(/ {2,}/g, ' ') + comment;
+                    .replace(/ {2,}/g, ' ') + comment
             formatDocument +=
                 !formatedText || formatedText.trim() == ''
                     ? formatedText
-                    : ' '.repeat(deep * options.tabSize) + formatedText;
+                    : ' '.repeat(deep * options.tabSize) + formatedText
             if (line !== document.lineCount - 1) {
-                formatDocument += '\n';
+                formatDocument += '\n'
             }
 
             if (oneCommandCode) {
-                oneCommandCode = false;
-                deep--;
+                oneCommandCode = false
+                deep--
             }
 
+            //start of indentation addition
             if (
+                purityText.includes("#if") ||
                 purityText.match(/#ifwinactive.*?\s/) ||
                 purityText.match(/#ifwinnotactive.*?\s/)
             ) {
-                deep++;
-                notDeep = false;
+                deep++
+                notDeep = false
+            }
+            //if hotkey::
+            if (!notHotkey) {
+                deep++
+                depthOfHotkey = deep
+                notDeep = false
             }
 
             if (purityText.includes("{")) {
-                if (!purityText.includes("{:")) {
-                    deep++;
+                if (notHotkey) {
+                    deep++
                 }
-                notDeep = false;
-            }
-
-            if (purityText.match(/:\s*$/)) {
-                deep++;
-                tagDeep = deep;
-                notDeep = false;
+                notDeep = false
             }
 
             if (notDeep) {
                 for (const oneCommand of FormatProvider.oneCommandList) {
-                    let temp: RegExpExecArray;
+                    let temp: RegExpExecArray
                     if (
                         (temp = new RegExp('\\b' + oneCommand + '\\b(.*)').exec(
                             purityText,
                         )) != null &&
                         !temp[1].includes('/')
                     ) {
-                        oneCommandCode = true;
-                        deep++;
-                        break;
+                        oneCommandCode = true
+                        deep++
+                        break
                     }
                 }
             }
         }
-        const result = [];
+        const result = []
         result.push(
             new vscode.TextEdit(
                 fullDocumentRange(document),
                 formatDocument.replace(/\n{2,}/g, '\n\n'),
             ),
-        );
-        return result;
+        )
+        return result
     }
 }
